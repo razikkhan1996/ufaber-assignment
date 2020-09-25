df6630f00ec168fb35ba42ea2c81bc229f79bc5f
Razik <razik99815@gmail.com>
2020-09-25 05:57:18 +0530

    Updates

---
+++ precache-manifest.21f32a348feef2f300e91020a45f8a28.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6387879051e04940bd3a06d8a4eea8c0",
+    "url": "/ufaber-assignment/index.html"
+  },
+  {
+    "revision": "cd8ce9a55277d7edd7a8",
+    "url": "/ufaber-assignment/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "09a3a8aadd39dd8cd978",
+    "url": "/ufaber-assignment/static/css/main.b31aefa4.chunk.css"
+  },
+  {
+    "revision": "cd8ce9a55277d7edd7a8",
+    "url": "/ufaber-assignment/static/js/2.41bcf07c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ufaber-assignment/static/js/2.41bcf07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a3a8aadd39dd8cd978",
+    "url": "/ufaber-assignment/static/js/main.05c24ff9.chunk.js"
+  },
+  {
+    "revision": "625e39f5acae8968d911",
+    "url": "/ufaber-assignment/static/js/runtime-main.6ab4f1eb.js"
+  },
+  {
+    "revision": "6e3bb00b166fa7977b650c924fb33085",
+    "url": "/ufaber-assignment/static/media/carou2.6e3bb00b.png"
+  },
+  {
+    "revision": "4912d9a543c8f9ed979bc00dac44628f",
+    "url": "/ufaber-assignment/static/media/container.4912d9a5.png"
+  },
+  {
+    "revision": "fc2dc51a33d04bcbaa227161032c5ee5",
+    "url": "/ufaber-assignment/static/media/content.fc2dc51a.png"
+  }
+]);
